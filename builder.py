75f45d652879b6446ddfe7b971d9cc51d6e653e2
alexjab <alexandre.jablon@gmail.com>
2014-11-16 10:29:01 +0100

    add version and help variables, enable multiple line commands, enable reading from a file, add an example in blueprint

--- builder.py
+++ builder.py
@@ -1,22 +1,65 @@
+import sys
+
 import yaml
 
-f = open('blueprint.yml', 'r')
+if len(sys.argv) > 1:
+  filename = sys.argv[1]
+  default_file = False
+else:
+  filename = 'blueprint.yml'
+  default_file = True
 
-doc = yaml.load(f)
-
-print doc
-
-toolbelt_name = doc['name']
-
-def search(tree, name):
-  if type(tree) == dict:
-    for command in tree:
-      search(tree[command], name+' '+command)
+try:
+  f = open(filename, 'r')
+  yfile = f.read()
+  f.close()
+except IOError as e:
+  if not default_file:
+    print 'Error: file not found:', filename
   else:
-    print name, tree
+    print 'Error: no blueprint file specified/found !'
+else:
+  blueprint = yaml.load(yfile)
 
-search(doc['commands'], toolbelt_name)
+  toolbelt_name = blueprint['name']
+  print 'TOOLBELT_NAME=' + toolbelt_name + ';'
+  toolbelt_version = blueprint['version']
+  print 'TOOLBELT_VERSION=' + toolbelt_version + ';'
 
-print toolbelt_name, '() {'
+  def build_cli(node, level):
+    spaces = ''.join(['  ']*(level+1))
+    if type(node) == dict:
+      index = 0
+      for param in node:
+        if index < 1:
+          print spaces + 'if [ "$'+str(level)+'"  = "'+param+'" ]; then'
+        else:
+          print spaces + 'elif [ "$'+str(level)+'" = "'+param+'" ]; then'
+        build_cli(node[param], level+1)
+        if index == len(node) - 1:
+          print spaces + 'else'
+          print spaces + '  ' + toolbelt_name + ' help'
+          print spaces + 'fi'
+        index+=1
+    else:
+      for line in node.split('\n'):
+        if line:
+          print spaces + line
 
-print '}'
+
+  print toolbelt_name + ' () {'
+
+  print '  ' + 'if [ "$1"  = "help" ]; then'
+
+  for line in blueprint['help'].split('\n'):
+    if line:
+      print '    ' + line
+
+  print '  ' + 'else'
+
+  build_cli(blueprint['commands'], 1)
+
+  print '  ' + 'fi'
+
+  print '}'
+
